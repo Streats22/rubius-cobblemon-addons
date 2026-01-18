6c8893c7dc3436f608dd32e5d63819a31da6dc7c
Streats1 <streatsdesign@outlook.com>
2026-01-18 18:42:04 +0100

    updated setup

--- src/main/java/nl/streats1/rubiusaddons/RubiusCobblemonAdditions.java
+++ src/main/java/nl/streats1/rubiusaddons/RubiusCobblemonAdditions.java
@@ -1,5 +1,6 @@
 package nl.streats1.rubiusaddons;
 
+import nl.streats1.rubiusaddons.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -32,6 +33,7 @@ public class RubiusCobblemonAdditions {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
